3c0aed59afe91af11f59a2c66a039d0831b15811
JakubKarasek <30701167+JakubKarasek@users.noreply.github.com>
2017-12-16 01:37:39 +0100

    Update Zadanie_3_konto_bankowe.py

--- J.Karasek/Zadanie_3_konto_bankowe.py
+++ J.Karasek/Zadanie_3_konto_bankowe.py
@@ -1,29 +1,65 @@
+x = input("Imię: ")
+y = input("Hasło: ")
+z = 2130
+
 while True:
-  x = input("Imię: ")
-  y = input("Hasło: ")
-  z = 2130
   if x == "Arnold" and y == "icra2013":
       print ("Saldo: ", z)
-      break
+      print("Jaką czynność chcesz wykonać?\nA - wpłata\nB - wypłata")
+      czynnosc = input("Czynność: ")
+      if czynnosc == "A":
+          wplata = int(input("Podaj kwotę: "))
+          if wplata > 0:
+              z += wplata
+              print("Twoje saldo wynosi teraz: ", z)
+              break
+          else:
+              print("Błędna kwota")
+              break
+      elif czynnosc == "B":
+          wyplata = int(input("Podaj kwotę: "))
+          if wyplata < z:
+              z -= wyplata
+              print("Twoje saldo wynosi teraz: ", z)
+              break
+          else:
+              print("Idź do pracy!")
+              break
   else:
       print("Błędne dane")
-  break
+      break
 
 
-print("Jaką czynność chcesz wykonać?\nA - wpłata\nB - wypłata")
-czynnosc = input("Czynność: ")
-if czynnosc == "A":
-    wplata = int(input("Podaj kwotę: "))
-    print("Twoje saldo wynosi teraz: ", z)
-elif czynnosc == "B":
-    wyplata = int(input("Podaj kwotę: "))
-    if wyplata < z :
-     z -= wyplata
-     print("Twoje saldo wynosi teraz: ", z)
-    else:
-     print("Idź do pracy!")
-else:
-    print("Błędne dane")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
