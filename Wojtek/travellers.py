bac9bcb92fb1d7d9a918832577725963a602c2b5
kowalskiw <kowalskiwojciech986@gmail.com>
2017-12-23 23:45:19 +0100

    jest opoznienie, sa sciany

--- Wojtek/travellers.py
+++ Wojtek/travellers.py
@@ -2,21 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 from matplotlib import patches as mpaths
+import json
 
-# program obrazujący w postaci animacji tor ruchu auta w kolejnych sekundach ruchu na podstawie położenia pocz
-# położenia końcowego i prędkości
-#
 #ZAD_DOM:
-#  - niech animacja kończy się gdy marsz skończy ostatni uciekinier
+#  - kilku z uciekinierów ma pik przed pierwszym krokiem (w lewym górnym rogu) - do naprawy
+#  - pobrać wszystkie możliwe dane z eggman.json, a nie deklarować w programie na stałe
 
 
 class Pedestrian:
-    def __init__(self, goals, velo):
+    def __init__(self, goals, velo, pre):
         self.traj = []
-        self.start = (0, 0)
         self.speed = velo
-        self.traj.append(self.start)
         self.goals = goals
+        self.pre_time = pre
 
     def create_vel_vector(self, actual, goal):
         vec_real = np.array(goal) - np.array(actual)  # wspolrzedne wektorow rzeczywistych aby z warunku brało goals_no
@@ -36,16 +34,20 @@ class Pedestrian:
 
     def do_it(self):
         goal_no = 0
-        position, vec_vel = self.create_trajectory(self.start, self.goals[goal_no])
+        step_no = 1
+        position = (self.goals[0][0], self.goals[0][1])
+        while self.pre_time-100 >= step_no:
+            self.traj.append(position)
+            step_no += 1
 
         while True:             #pętla tworząca trajektorię
             try:
-                if np.linalg.norm(self.goals[goal_no] - np.array(position)) <= self.speed:
+                if np.linalg.norm(self.goals[goal_no] - np.array(position)) <= self.speed/2:
                     goal_no += 1
                 position, vec_vel = self.create_trajectory(position, self.goals[goal_no])
             except:
                 break
-
+        print(self.traj)
         return self.traj
 
     def chart(self):
@@ -60,8 +62,7 @@ class Pedestrians:
         self.peds = []
 
     def add_pedestrians(self, ped):
-        self.peds.extend(ped)
-        print(self.peds)
+        self.peds.append(ped)
 
     def get_trajectories(self):
         trajectories = []
@@ -85,7 +86,7 @@ class Pedestrians:
                 except:
                     step.append(trajs[j][-1])
             temp_traj.append(step)          #do tego momentu tworzy odpowiednią trajektorię
-        print(temp_traj)
+        print('trajektoria posortowana wg krokow:', temp_traj)
 
         foo = zip(*temp_traj)               #rysuje trajektorie
         for i in foo:
@@ -98,15 +99,16 @@ class Pedestrians:
 class Animation:
     def __init__(self, pedest):
         self.fig = plt.figure()
-        self.ax = plt.axes(xlim=(-100, 100), ylim=(-100, 100))
+        self.ax = plt.axes(xlim=(-1, 60), ylim=(-1, 20))
         self.n_frames = 0
         self.trial = []
         self.ang = 0
+        self.create_walls()
 
         self.trajectory = pedest.get_chart()
         self.n_frames = len(self.trajectory)
 
-        elipses = [mpaths.Ellipse(i, width=5, height=5, angle=self.ang) for i in
+        elipses = [mpaths.Ellipse(i, width=1, height=1, angle=self.ang) for i in
                    self.trajectory[0]]      # stworzenie kształtu i przypisanie mu cech (wymiary, współ początkowych)
         [self.trial.append(self.ax.add_patch(elipses[i])) for i in range(len(elipses))]
 
@@ -125,14 +127,33 @@ class Animation:
                                      interval=n_interval, blit=True)
         plt.show()
 
+    def create_walls(self):
+        for i in obst:                                      # kreślenie ścian budynku
+            corners_x = [i[0][0], i[1][0], i[2][0], i[3][0]]
+            corners_y = [i[0][1], i[1][1], i[2][1], i[3][1]]
+            plt.plot(corners_x, corners_y, "r", lw=3)
 
-d = Pedestrian([(10, 10), (60, 30), (9, 12)], 2)
-e = Pedestrian([(8, 11), (-5, 39), (18, 72)], 1)
-f = Pedestrian([(-5, -34), (30, 67), (-40, 97)], 2)
-g = Pedestrian([(-25, 12), (-15, -8), (6, 9)], 1)
-peds = [d, e, f, g]
+
+def change_data(eggman_ped):
+    vel = eggman_ped["H_SPEED"]
+    goals = []
+    goals.append(eggman_ped['ORIGIN'])
+    goals.extend(eggman_ped['ROADMAP'])
+    pre = eggman_ped["PRE_EVACUATION"]
+
+    return goals, vel, pre
+
+
+file1 = open("//home/wojtas/PythonFiles/main-repository/Python - zadania/ewakuacja2/eggman.json", 'r')
+first_floor = json.load(file1)["1"]["EVACUEES"]
+file2 = open("//home/wojtas/PythonFiles/main-repository/Python - zadania/ewakuacja2/geom.json", "r")
+obst = json.load(file2)["obstacles"]["1"]
+
+
 a = Pedestrians()
-a.add_pedestrians(peds)
+for i in first_floor:
+    temp_ped = Pedestrian(*change_data(first_floor[i]))
+    a.add_pedestrians(temp_ped)
 
 x = Animation(a)
 x.do_animation(100)
