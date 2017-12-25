5281c3ffbaae2095a26e745900d8c9d3211e6205
JakubKarasek <30701167+JakubKarasek@users.noreply.github.com>
2017-12-25 18:33:39 +0100

    Add files via upload

---
+++ J.Karasek/numery_telefonu.py
@@ -0,0 +1,48 @@
+contacts = {"Arek": 506234556,
+            "Basia": 607456233,
+            "Maja": 589311911,
+            "Martyna": 778456332,
+            "Paweł": 501223493
+            }
+
+def phone_number(name):
+   return("Numer telefonu to: %s" %(contacts[name]))
+
+def sort_by_key():
+    sorted_by_key_contacts = sorted(contacts.items(), key = lambda t: t[0])
+    print(sorted_by_key_contacts)
+
+play = ''
+while play != "zakończ":
+
+  print("KONTAKTY:")
+  for key in contacts:
+      print(key)
+  name = input("Podaj imię: ")
+  if name in contacts:
+    print(phone_number(name))
+  else:
+      print("Nie ma takiego kontaktu.")
+  print("Jaką czynność chcesz wykonać? \nA - dodaj nowy kontakt B - usuń bieżący kontakt C - wyjdź ")
+  action = input("Czynność: ")
+  if action == "A":
+      new_name = input("Wprowadź imię: ")
+      new_number = input("Wprowadź numer: ")
+      new_element = contacts[new_name] = new_number
+      play = input("Dodałeś nowy konakt,jeśli chcesz ponownie wyświetlić numery, wciśnij enter,jeśli nie, napisz 'zakończ'")
+  elif action == "B":
+    delete = input("Który kontakt chcesz usunąć? ")
+    if delete in contacts:
+      contacts.pop(delete)
+      play = input("Usunąłeś bieżący konakt,jeśli chcesz ponownie wyświetlić numery, wciśnij enter,jeśli nie, napisz 'zakończ'")
+    else:
+      print("Nie ma takiego kontaktu.")
+      play = input("Jeśli chcesz ponownie wyświetlić numery, wciśnij enter,jeśli nie, napisz 'zakończ'")
+  else:
+    play = "zakończ"
+    print("Do widzenia!")
+
+
+
+#posortować kontakty po dodaniu
+
